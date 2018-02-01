d62a82359bffc87b48c9e67132fc95b0f565f883
BertShin <bertshin08@gmail.com>
2018-02-01 03:17:29 -0800

    Force Personal Links to open in new tab

--- frontend/components/personal_plugs.jsx
+++ frontend/components/personal_plugs.jsx
@@ -5,9 +5,9 @@ const PersonalPlugs = () => {
 
   return (
     <div className="main-plug animated fadeIn">
-      <a id="git" href="https://github.com/BertShin"></a>
-      <a id="linked" href="https://www.linkedin.com/in/albert-w-shin-020311154/"></a>
-      <a id="angel" href="https://angel.co/bertshin?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"></a>
+      <a target="_blank" id="git" href="https://github.com/BertShin"></a>
+      <a target="_blank" id="linked" href="https://www.linkedin.com/in/albert-w-shin-020311154/"></a>
+      <a target="_blank" id="angel" href="https://angel.co/bertshin?al_content=view+your+profile&al_source=transaction_feed%2Fnetwork_sidebar"></a>
     </div>
   );
 };
