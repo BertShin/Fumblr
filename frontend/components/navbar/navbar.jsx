1da09efae9aeca1fc56d0760bf40169b11361391
BertShin <bertshin08@gmail.com>
2018-02-01 02:50:38 -0800

    Another Heroku Retry

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import SearchBarContainer from './searchbar_container';
-
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleLogout = this.handleLogout.bind(this);
-  }
-  
-  handleLogout(e) {
-    e.preventDefault;
-    this.props.logout();
-  }
-
-  render () {
-    return (
-      <div>
-        <SearchBarContainer />
-        <nav className='navbar-links'>
-          <button onClick={(e) => this.handleLogout(e)}>Logout</button>
-          <a href='http://nba.com'>NBA logo here </a>
-          <a href='https://www.nfl.com/'>NFL logo here </a>
-          <a href='http://www.tennis.com/scores/#/?_k=dyqsz8'>Tennis Logo Here </a>
-          <a href='http://www.espn.com/soccer/scoreboard'>Soccer Ball here </a>
-          <a href="https://www.tumblr.com/">tumblr plug here </a>
-        </nav>
-      </div>
-    );
-  }
-}
-
-
-export default NavBar;
+// import React from 'react';
+// import { Link, Redirect } from 'react-router-dom';
+// import SearchBarContainer from './searchbar_container';
+//
+// class NavBar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.handleLogout = this.handleLogout.bind(this);
+//   }
+//
+//   handleLogout(e) {
+//     e.preventDefault;
+//     this.props.logout();
+//   }
+//
+//   render () {
+//     return (
+//       <div>
+//         <SearchBarContainer />
+//         <nav className='navbar-links'>
+//           <button onClick={(e) => this.handleLogout(e)}>Logout</button>
+//           <a href='http://nba.com'>NBA logo here </a>
+//           <a href='https://www.nfl.com/'>NFL logo here </a>
+//           <a href='http://www.tennis.com/scores/#/?_k=dyqsz8'>Tennis Logo Here </a>
+//           <a href='http://www.espn.com/soccer/scoreboard'>Soccer Ball here </a>
+//           <a href="https://www.tumblr.com/">tumblr plug here </a>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
+//
+//
+// export default NavBar;
