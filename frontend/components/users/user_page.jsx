b4f70ad625dd70127b83017067f1d776a14bbb9f
BertShin <bertshin08@gmail.com>
2018-02-07 12:31:24 -0800

    Fix a bootstrap issue where currentUser slice of state was not returning enough information. Implemented a selector for User specific posts

---
+++ frontend/components/users/user_page.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+//
+// class UserPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//   }
+//
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
+//
+// export default UserPage;
---
+++ frontend/reducers/selectors.js
@@ -0,0 +1,11 @@
+
+export const getPosts = ({ posts, userId }) => {
+  const iPosts = Object.values(posts);
+  const desiredPosts = [];
+  for (let i = 0; i < iPosts.length; i++) {
+    if (iPosts[i].user_id === userId) {
+      desiredPosts.push(iPosts[i]);
+    }
+  }
+  return desiredPosts;
+};
