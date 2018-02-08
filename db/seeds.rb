f86fb79675ae1ed6a9e38b718d4fbfbc5c380fe5
BertShin <bertshin08@gmail.com>
2018-02-07 17:25:46 -0800

    Alter seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+User.create!([
+  {username: "SerenaWilliams", email: "serenawilliams@gmail.com", session_token: "D2BHwQ7_S-8J0Aq6sQB5zXE", password_digest: "$2a$10$e6RKTKsKpwESh75no1Ie2u0UZpMQUwtDu3w7KU3Pmf2sdA3ae5GYq", image_url: "https://specials-images.forbesimg.com/imageserve/575706a4a7ea43396db27353/416x416.jpg?background=000000&cropX1=138&cropX2=676&cropY1=126&cropY2=664"},
+  {username: "Alberto", email: "albert@gmail.com", session_token: "UvApUzYg-X1QTSKq78O2Zm0", password_digest: "$2a$10$3Q9E5yvCJw0JydGJA3qlg.hH5eP6l6/uJTfqBDKyAj6dWJJh0nNrq", image_url: "https://usatftw.files.wordpress.com/2017/07/kobe-champion.jpg"},
+  {username: "HelloGuest", email: "bestguest@gmail.com", session_token: "1P-EkO2Rj9IiqsK-hhBFW8M", password_digest: "$2a$10$q6Y5qAyRSBnEXyaYRfousu6vNzC9Ag6wyieqEOgZ9GlFyN/lkuzTK", image_url: "https://www.limestone.edu/sites/default/files/user.png"},
+  {username: "MiaHamm", email: "miahamm@gmail.com", session_token: "53uxAYT8fqhri37dul9XnB8", password_digest: "$2a$10$FvpXzGwpqL8ZqPX9OPUDGe0KI/vHrkAmlRo.TA2deo0de2wHGP.72", image_url: "https://static1.squarespace.com/static/52f13ae1e4b08a02b16034fb/t/52fd6734e4b09b324c795621/1392338742015/Mia_Hamm.jpg?format=2500w"},
+  {username: "BertShin", email: "bertshin@gmail.com", session_token: "SBUbOXxR9tCuzaKH5jhsaNE", password_digest: "$2a$10$WzH3D5ftUJUtRyi4K.KJZONGOYfEtYaM976M1amxE/QKa6I8e93zu", image_url: "https://pbs.twimg.com/profile_images/950664597366714369/M4PmYTBN.jpg"}
+  ])
+  
 Post.create!([
   {title: "The person that said winning isn’t everything", user_id: 1, description: "Mia Hamm", image_url: nil, likes: nil, content: "never won anything", content_type: "Quote"},
   {title: "Still one of the best dunks of all time!", user_id: 2, description: "By KB The GOAT", image_url: "https://media.giphy.com/media/VvZSI1pH1b2Ra/giphy.gif", likes: nil, content: nil, content_type: "Text"},
@@ -29,11 +37,4 @@ Post.create!([
   {title: "I've had to learn to fight my whole life", user_id: 4, description: "Serena Williams AKA myself", image_url: nil, likes: nil, content: "got to learn to keep smiling. If you smile, things will work out", content_type: "Quote"},
   {title: "Hittin 3's @ the 24 like", user_id: 3, description: "bang bang", image_url: "https://media.giphy.com/media/3ohs4xFl0wj0FehtfO/giphy.gif", likes: nil, content: "", content_type: "Text"},
   {title: "GOAT", user_id: 3, description: "", image_url: "https://media.giphy.com/media/L5ohDduqx0dwY/giphy.gif", likes: nil, content: "", content_type: "Text"}
-])
-User.create!([
-  {username: "SerenaWilliams", email: "serenawilliams@gmail.com", session_token: "D2BHwQ7_S-8J0Aq6sQB5zXE", password_digest: "$2a$10$e6RKTKsKpwESh75no1Ie2u0UZpMQUwtDu3w7KU3Pmf2sdA3ae5GYq", image_url: "https://specials-images.forbesimg.com/imageserve/575706a4a7ea43396db27353/416x416.jpg?background=000000&cropX1=138&cropX2=676&cropY1=126&cropY2=664"},
-  {username: "Alberto", email: "albert@gmail.com", session_token: "UvApUzYg-X1QTSKq78O2Zm0", password_digest: "$2a$10$3Q9E5yvCJw0JydGJA3qlg.hH5eP6l6/uJTfqBDKyAj6dWJJh0nNrq", image_url: "https://usatftw.files.wordpress.com/2017/07/kobe-champion.jpg"},
-  {username: "HelloGuest", email: "bestguest@gmail.com", session_token: "1P-EkO2Rj9IiqsK-hhBFW8M", password_digest: "$2a$10$q6Y5qAyRSBnEXyaYRfousu6vNzC9Ag6wyieqEOgZ9GlFyN/lkuzTK", image_url: "https://www.limestone.edu/sites/default/files/user.png"},
-  {username: "MiaHamm", email: "miahamm@gmail.com", session_token: "53uxAYT8fqhri37dul9XnB8", password_digest: "$2a$10$FvpXzGwpqL8ZqPX9OPUDGe0KI/vHrkAmlRo.TA2deo0de2wHGP.72", image_url: "https://static1.squarespace.com/static/52f13ae1e4b08a02b16034fb/t/52fd6734e4b09b324c795621/1392338742015/Mia_Hamm.jpg?format=2500w"},
-  {username: "BertShin", email: "bertshin@gmail.com", session_token: "SBUbOXxR9tCuzaKH5jhsaNE", password_digest: "$2a$10$WzH3D5ftUJUtRyi4K.KJZONGOYfEtYaM976M1amxE/QKa6I8e93zu", image_url: "https://pbs.twimg.com/profile_images/950664597366714369/M4PmYTBN.jpg"}
 ])
