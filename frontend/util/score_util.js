dd9a256488035d2454b7d17f798c3d77641f1b31
BertShin <bertshin08@gmail.com>
2018-03-20 16:36:54 -0700

    Clean up score_util

--- frontend/util/score_util.js
+++ frontend/util/score_util.js
@@ -1,9 +1,10 @@
-
 export const fetchGame = (sport, forDate) => {
   return $.ajax({
+    // Including a Sport when later sport data have become available
     url: `https://api.mysportsfeeds.com/v1.2/pull/${sport}/2016-2017-regular/scoreboard.json?fordate=${forDate}`,
     method: 'GET',
     headers: {
+      // Basic&Free Acc
       "Authorization": "Basic " + btoa("Student" + ":" + "AmazingPW")
     },
     dataType: 'json',
