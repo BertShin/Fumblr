4062221e6e6f9eda3a6fe03deb8dab3c7cab0cf9
BertShin <bertshin08@gmail.com>
2018-03-20 16:37:41 -0700

    Clean up Giphy API util and Route Util

--- frontend/util/giphy_api_util.js
+++ frontend/util/giphy_api_util.js
@@ -1,5 +1,3 @@
-
-
 export const fetchSearchGiphys = (searchTerm) => {
   return $.ajax({
     url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=12&rating=pg-13`,
